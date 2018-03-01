135f9692ea12d6713b7416823207aedd02690e17
betosousa <avss@cin.ufpe.br>
2018-03-01 18:23:15 -0300

    adjusting plot labels

--- plots/activityPlot.py
+++ plots/activityPlot.py
@@ -9,12 +9,12 @@ f = open('input.csv', 'r')
 for line in f:
 	commit = line.split(',')
 	add.append(int(commit[0]))
-	rem.append(-int(commit[1]))
+	rem.append(int(commit[1]))
 	dates.append(commit[2])
 f.close()
  
-addBar = graph.Bar(x = dates, y = add, name = 'Added', marker = dict(color='green')) 
-remBar = graph.Bar(x = dates, y = rem, name = 'Removed', marker = dict(color='red'))
+addBar = graph.Bar(x = dates, y = add, name = 'Added', marker = dict(color='green'), text = add, textposition = 'outside') 
+remBar = graph.Bar(x = dates, y = [-r for r in rem], name = 'Removed', marker = dict(color='red'), text = rem, textposition = 'outside')
 data = [addBar, remBar]
 
 layout = graph.Layout(
@@ -24,7 +24,7 @@ layout = graph.Layout(
 
 figure = graph.Figure(data = data, layout = layout)
 
-plotly.plot(figure, image='png', auto_open=False,
+plotly.plot(figure, image='png', auto_open=True,
 			filename = 'activity_history.html',
 			image_filename='activity_history')
 			

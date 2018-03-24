fc4ae2795ba4eacdc97e49352d4a75e8a0a45732
betosousa <avss@cin.ufpe.br>
2018-03-24 13:18:01 -0300

    filling modifications list

--- src/main/java/android/diff/ComponentDiff.java
+++ src/main/java/android/diff/ComponentDiff.java
@@ -13,6 +13,7 @@ public abstract class ComponentDiff {
 	
 	private List<Component> added = new ArrayList<>();
 	private List<Component> removed = new ArrayList<>();
+	private List<Component> modified = new ArrayList<>();
 	
 	private List<Component> committed = new ArrayList<>();
 	private List<Component> beforeCommit = new ArrayList<>();
@@ -39,11 +40,29 @@ public abstract class ComponentDiff {
 	
 	protected abstract <T extends Component> List<T> getComponents(AndroidManifest manifest);
 	
+	
+	/**
+	 * As equals only compares name and type, 
+	 * we compare the others attributes to check for modifications
+	 * 
+	 * @param component Committed Component to be checked with its previous version.
+	 *  
+	 * @return true if the component has the same name and type of its previous version 
+	 * 			and any modified attribute
+	 */
+	private boolean isModification(Component component){
+		int index = beforeCommit.indexOf(component);
+		Component original = beforeCommit.get(index); 
+		return component.isModificationOf(original);
+	}
+	
 	private void fillAddedAndRemoved(){
 		if (committed.size() > 0 || beforeCommit.size() > 0) {
 			for (Component component : committed) {
 				if (!beforeCommit.contains(component)) {
 					added.add(component);
+				} else if(isModification(component)) {
+					modified.add(component);
 				}
 			}
 			for (Component component : beforeCommit) {
@@ -58,32 +77,20 @@ public abstract class ComponentDiff {
 		return added;
 	}
 
-	public void setAdded(List<Component> added) {
-		this.added = added;
-	}
-
 	public List<Component> getRemoved() {
 		return removed;
-	}
-
-	public void setRemoved(List<Component> removed) {
-		this.removed = removed;
 	}
 
 	public List<Component> getCommitted() {
 		return committed;
 	}
 
-	public void setCommitted(List<Component> committed) {
-		this.committed = committed;
-	}
-
 	public List<Component> getBeforeCommit() {
 		return beforeCommit;
 	}
 
-	public void setBeforeCommit(List<Component> beforeCommit) {
-		this.beforeCommit = beforeCommit;
+	public List<Component> getModified() {
+		return modified;
 	}
 
 }

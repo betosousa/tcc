b455dcf11600ee61d7ecc40a6aa6f6030c1fa733
betosousa <avss@cin.ufpe.br>
2018-03-24 13:21:26 -0300

    adding modifications into plots and renaming activityPlot to componentPlot

---
+++ plots/componentHistoryPlot.py
@@ -0,0 +1,35 @@
+import plotly.offline as plotly
+import plotly.graph_objs as graph
+
+def plot_history(component_type):
+	add = []
+	rem = []
+	mod = []
+	dates = []
+	f = open(component_type + 'Driller.csv', 'r')
+	for line in f:
+		commit = line.split(',')
+		add.append(int(commit[0]))
+		rem.append(int(commit[1]))
+		mod.append(int(commit[2]))
+		dates.append(commit[3])
+	f.close()
+	addBar = graph.Bar(x = dates, y = add, name = 'Added', marker = dict(color='green'), text = add, textposition = 'outside') 
+	remBar = graph.Bar(x = dates, y = [-r for r in rem], name = 'Removed', marker = dict(color='red'), text = rem, textposition = 'outside')
+	modBar = graph.Bar(x = dates, y = mod, name = 'Modified', marker = dict(color='blue'), text = mod, textposition = 'outside') 
+	data = [addBar, remBar, modBar]
+	layout = graph.Layout(
+		title = component_type + ' Commit History',
+		barmode = 'relative',
+		)
+	figure = graph.Figure(data = data, layout = layout)
+	plotly.plot(figure, image='png', auto_open=True,
+				filename = component_type + '_history.html',
+				image_filename = component_type + '_history')
+
+
+if __name__ == '__main__':
+	plot_history('activity')
+	plot_history('service')
+	plot_history('broadcastReceiver')
+	plot_history('contentProvider')
